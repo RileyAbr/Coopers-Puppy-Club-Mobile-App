f60b0952c8016458fa088ae36b92c206793b0f8a
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-10-29 00:30:39 -0500

    Solved Prettier+ESLint issues

--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    } else {
-        return next();
-    }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, X-Requested-With, Range"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  } else {
+    return next();
+  }
 });
 
 app.listen(process.env.PORT, function () {
-    console.log(`Cooper's Puppy Club server is listening on port: ${process.env.PORT}`);
+  console.log(
+    `Cooper's Puppy Club server is listening on port: ${process.env.PORT}`
+  );
 });
---
+++ server/services/mongodb.service.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+let count = 0;
+
+const uri = process.env.MONGO_URI;
+
+const options = {
+    autoIndex: false, // Don't build indexes
+    poolSize: 10, // Maintain up to 10 socket connections
+    bufferMaxEntries: 0,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const connectWithRetry = () => {
+    console.log("MongoDB connection with retry");
+    mongoose
+        .connect(uri, options)
+        .then(() => {
+            console.log("MongoDB is connected");
+        })
+        .catch((err) => {
+            console.log(
+                "MongoDB connection unsuccessful, retry after 5 seconds.",
+                ++count
+            );
+            setTimeout(connectWithRetry, 5000);
+        });
+};
+
+connectWithRetry();
+
+exports.mongoose = mongoose;
