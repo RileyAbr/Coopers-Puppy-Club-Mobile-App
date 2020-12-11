9277184559c727a9347d56db92b0b7b9e7e80054
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-12-11 16:37:11 -0600

    Rudimentary check-in features now work

--- app/screens/Home/Home.js
+++ app/screens/Home/Home.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Button } from 'react-native-elements';
+import React from "react";
+import { View } from "react-native";
+import { Button } from "react-native-elements";
+import axios from "axios";
 
 const ToeBeanSize = 90;
+const FootSize = 210;
 
-const Home = ({ navigation }) => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+const handleCheckInPress = () => {
+    const checkIn = {
+        parkID: "5fbb1917a632915e007ad6f3",
+        userID: "5fbb210b4826fc5d08dc9381",
+        dogID: "5fbb2409e016be04e036921d",
+    };
+
+    const config = {
+        headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZmJiMjQ5OGM5ZThmYzVlOWNiNzFlZDciLCJlbWFpbCI6ImFyaWNoQGdtYWlsLmNvbSIsInByb3ZpZGVyIjoiZW1haWwiLCJuYW1lIjoiQW5kcmVhIFJpY2hhcmQiLCJyZWZyZXNoS2V5IjoiUjVqcktzOEZnNlBCdnB3Y0NibG96dz09IiwiaWF0IjoxNjA3NzE2OTU1fQ.4rwrwfVrdwMWgV8RRm9Ftw-s_OzTPbRVOu2pwTC8tR8`,
+        },
+    };
+
+    axios
+        .post(`https://coopers-puppy-club-server.herokuapp.com/checkins`, checkIn, config)
+        .then((res) => {
+            alert(res.data);
+        });
+};
+
+const Home = () => (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <View
             style={{
-                width: '100%',
-                flexDirection: 'row',
-                justifyContent: 'center',
+                width: "100%",
+                flexDirection: "row",
+                justifyContent: "center",
                 marginBottom: -35,
             }}
         >
@@ -18,7 +40,7 @@ const Home = ({ navigation }) => (
                 style={{
                     width: ToeBeanSize,
                     height: ToeBeanSize,
-                    backgroundColor: 'blue',
+                    backgroundColor: "skyblue",
                     borderRadius: ToeBeanSize,
                     marginTop: 60,
                 }}
@@ -27,7 +49,7 @@ const Home = ({ navigation }) => (
                 style={{
                     width: ToeBeanSize,
                     height: ToeBeanSize,
-                    backgroundColor: 'blue',
+                    backgroundColor: "skyblue",
                     borderRadius: ToeBeanSize,
                     marginLeft: 0,
                     marginRight: 5,
@@ -37,7 +59,7 @@ const Home = ({ navigation }) => (
                 style={{
                     width: ToeBeanSize,
                     height: ToeBeanSize,
-                    backgroundColor: 'blue',
+                    backgroundColor: "skyblue",
                     borderRadius: ToeBeanSize,
                     marginLeft: 5,
                     marginRight: 0,
@@ -47,7 +69,7 @@ const Home = ({ navigation }) => (
                 style={{
                     width: ToeBeanSize,
                     height: ToeBeanSize,
-                    backgroundColor: 'blue',
+                    backgroundColor: "skyblue",
                     borderRadius: ToeBeanSize,
                     marginTop: 60,
                 }}
@@ -55,10 +77,11 @@ const Home = ({ navigation }) => (
         </View>
         <Button
             title="Check-in"
+            onPress={handleCheckInPress}
             buttonStyle={{
-                width: 210,
-                height: 210,
-                borderRadius: 200,
+                width: FootSize,
+                height: FootSize,
+                borderRadius: FootSize,
             }}
         />
     </View>
