499915dd8fe870c96c8e65303ec525cd5b8a2368
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-12-10 23:55:15 -0600

    Nav Tabs now have icons

--- app/App.js
+++ app/App.js
@@ -4,6 +4,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import Home from './screens/Home';
 import ParksList from './screens/ParksList';
 import Profile from './screens/Profile';
@@ -11,7 +13,35 @@ import Profile from './screens/Profile';
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => (
-    <Tab.Navigator>
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                switch (route.name) {
+                    case 'Home':
+                        iconName = 'paw';
+                        break;
+                    case 'ParksList':
+                        iconName = 'tree';
+                        break;
+                    case 'Profile':
+                        iconName = 'user';
+                        break;
+                    default:
+                        iconName = 'question';
+                        break;
+                }
+
+                // You can return any component that you like here!
+                return <Icon name={iconName} size={size} color={color} />;
+            },
+        })}
+        tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        }}
+    >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="ParksList" component={ParksList} />
         <Tab.Screen name="Profile" component={Profile} />
