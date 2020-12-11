9ccc47d717dd15900317ce2bd8c56be8214d8dac
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-12-11 16:03:38 -0600

    Basic Check-In button created

--- app/screens/Home/Home.js
+++ app/screens/Home/Home.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const Home = ({ navigation }) => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <View
+            style={{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginBottom: -15,
+            }}
+        >
+            <View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'black',
+                    borderRadius: 100,
+                    marginTop: 45,
+                }}
+            />
+            <View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'black',
+                    borderRadius: 100,
+                    marginVertical: 0,
+                    marginHorizontal: 10,
+                }}
+            />
+            <View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'black',
+                    borderRadius: 100,
+                    marginTop: 45,
+                }}
+            />
+        </View>
+        <Button
+            title="Check-in"
+            buttonStyle={{
+                width: 210,
+                height: 210,
+                borderRadius: 200,
+            }}
+        />
     </View>
 );
 
